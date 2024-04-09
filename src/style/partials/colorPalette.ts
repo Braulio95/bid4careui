7ac183c64add786467605172124c5c101a1d94e2
Braulio95 <braulioestradaalfonso@hotmail.com>
2024-04-08 21:32:31 -0600

    agregando configuraciones iniciales, paleta de colores y spacings

---
+++ src/style/partials/colorPalette.ts
@@ -0,0 +1,24 @@
+const enum colorPalette {
+  // Main Brand Colors
+  primary = '#7CA4DE', // Define a specific shade of blue for the primary color
+  secondary = '#B8D6FF',
+  // State colors
+  info = '#2F80ED',
+  success = '#27AE60',
+  warning = '#E2B93B',
+  error = '#EB5757',
+  // Black Tints 
+  black1 = '#000000',
+  black2 = '#1D1D1D',
+  black3 = '#282828',
+  white = '#FFFFFF',
+  // Grey Tints 
+  grey1 = '#333333',
+  grey2 = '#4F4F4F',
+  grey3 = '#828282',
+  grey4 = '#BDBDBD',
+  grey5 = '#E0E0E0',
+}
+
+// Export the colorPalette enum for use in other files
+export { colorPalette };
---
+++ src/style/partials/spacings.ts
@@ -0,0 +1,18 @@
+const enum Spacings {
+  spacing0 = 0,
+  spacing1 = 8,
+  spacing2 = 16,
+  spacing3 = 24,
+  spacing4 = 32,
+  spacing5 = 40,
+  spacing6 = 56,
+  spacing7 = 72,
+  spacing8 = 80,
+  spacing9 = 96,
+  spacing10 = 120,
+}
+
+const spacingScale = 1;
+
+// Export the spacingsList enum for use in other files
+export { Spacings, spacingScale };
