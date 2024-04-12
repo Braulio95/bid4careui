b1e13a595d82ab86b85385c0aea806ad6881cd1c
Braulio95 <braulioestradaalfonso@hotmail.com>
2024-04-11 18:49:31 -0600

    agregando textfield

--- src/components/B4CTextfield/B4CTextfield.stories.tsx
+++ src/components/B4CTextfield/B4CTextfield.stories.tsx
@@ -14,12 +14,15 @@ const meta = {
   // More on argTypes: https://storybook.js.org/docs/api/argtypes
 
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
-  args: { onClick: fn() },
 } satisfies Meta<typeof B4CTextfield>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Primary: Story = {
-  args: {},
+  args: { label: "Textfield" },
+};
+
+export const Secondary: Story = {
+  args: { label: "Textfield", isMultiline: true },
 };
--- src/components/B4CTextfield/B4CTextfield.tsx
+++ src/components/B4CTextfield/B4CTextfield.tsx
@@ -1,6 +1,69 @@
-import { TextField } from "@mui/material";
-import React from "react";
+import { colorPalette } from "@/style/partials/colorPalette";
+import { spacings } from "@/style/partials/spacings";
+import { Box, Hidden, InputLabel, TextField, Typography } from "@mui/material";
+import React, { ChangeEventHandler, FocusEventHandler } from "react";
 
-export const B4CTextfield = () => {
-  return <TextField>B4CTextfield</TextField>;
+interface IB4CTextfieldProps {
+  disabled?: boolean;
+  error?: boolean;
+  helper?: string;
+  id?: string;
+  isMultiline?: boolean;
+  label: string;
+  placeholder?: string;
+  touched?: boolean;
+  onBlur?: FocusEventHandler<HTMLInputElement>;
+  onChange?: ChangeEventHandler<HTMLInputElement>;
+}
+
+export const B4CTextfield = ({
+  disabled,
+  error,
+  helper,
+  id,
+  label,
+  isMultiline,
+  placeholder,
+  touched,
+}: IB4CTextfieldProps) => {
+  return (
+    <Box display="flex" flexDirection="column">
+      <InputLabel htmlFor={id} sx={{ marginBottom: spacings.spacing1 }}>
+        <Typography variant="body-normal-bold" color={colorPalette.black1}>
+          {label}
+        </Typography>
+      </InputLabel>
+      <TextField
+        inputProps={{
+          sx: {
+            paddingBlock: "16px",
+            paddingLeft: `${16 * 2}px`,
+            "&::placeholder": {
+              color: colorPalette.black1, // Modifica el color del placeholder
+              opacity: 1,
+              fontSize: "16px", // Modifica el estilo del placeholder
+              // Otros estilos que desees aplicar al placeholder
+            },
+          },
+        }}
+        id={id}
+        disabled={disabled}
+        multiline={isMultiline}
+        placeholder={placeholder}
+      />
+
+      {helper && (
+        <Box display="flex" justifyContent="space-between" paddingTop="8px">
+          {helper && (
+            <Typography
+              variant="body-small"
+              color={error && touched ? colorPalette.error : colorPalette.grey4}
+            >
+              {helper}
+            </Typography>
+          )}
+        </Box>
+      )}
+    </Box>
+  );
 };
